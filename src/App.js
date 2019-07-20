624bb21359ececcc13c79edc1851796b85139dca
angel-torres <alas4224@gmail.com>
2019-07-19 23:49:20 -0500

    bring in firebase into firebase.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import start from './firebase';
 
 function App() {
+
+  useEffect( () => {
+    start();
+  });
+
   return (
     <div className="App">
       <button>Login with firebase</button>
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+import firebase from 'firebase';
+const firebaseui = 'firebaseui';
+
+function start() {
+    console.log("firebase", firebase);
+    console.log("firebaseui", firebaseui);
+}
+
+export default start;
