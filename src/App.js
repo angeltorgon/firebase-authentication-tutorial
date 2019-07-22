4d62c688daf1f55b0c8a2942ff1d27e705039598
angel-torres <alas4224@gmail.com>
2019-07-21 23:50:06 -0500

    was able to get information from user after he is logged in

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import React, { useEffect } from "react";
 import "./App.css";
 import start from "./firebase";
+import firebase from "firebase";
 
 function App() {
-    useEffect(() => {});
+    useEffect(() => {
+        firebase.auth()
+        .getRedirectResult()
+        .then(function(result) {
+            if (result.credential) {
+              // This gives you a Google Access Token. You can use it to access the Google API.
+              var token = result.credential.accessToken;
+              // ...
+            }
+            // The signed-in user info.
+            var user = result.user;
+            console.log(user);
+            console.log(token);
+          }).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            // ...
+          });
+    });
 
     const handleClick = () => {
         start();
